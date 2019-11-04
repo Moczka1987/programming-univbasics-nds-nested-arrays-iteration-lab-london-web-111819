3727790aa5ee41ddc744478fa3b59c4b44271792
Mariola Pasko <paskomariola@gmail.com>
2019-11-04 16:15:35 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,13 +1,5 @@
 def join_ingredients(src)
-row = 0
-  array = []
-  while row < src.length do
-    array.push("I love #{src[row][0]} and #{src[row][1]} on my pizza")
-    row += 1
-  end
 
-array
-end	
 end
 
 def find_greater_pair(src)
